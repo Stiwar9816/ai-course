d15580ebadd44022d344b9425861b081ae670cb6
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2024-10-16 09:14:46 -0500

    first commit

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./configs/schema.jsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://magicdb_owner:koOuDTZA65MG@ep-odd-fog-a55tr55b.us-east-2.aws.neon.tech/magicdb?sslmode=require' 
-    }
-  };
+  schema: "./configs/schema.jsx",
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING
+  }
+};
